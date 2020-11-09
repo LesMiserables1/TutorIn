0f3d400a157b7fcdf85b01fd92573b9ce6920517
LesMiserables1 <liemandre20@gmail.com>
2020-11-09 07:39:01 +0700

    add functionality

--- class_diagram.py
+++ class_diagram.py
@@ -1,3 +1,6 @@
+from datetime import date
+from datetime import datetime
+
 class User:
     username = None
     _password = None
@@ -16,28 +19,35 @@ class User:
         
     def login(self, username,password):
         if (self.username == username) and (self._password == password):
+            print('Login Succeeded')
             return True
         else:
+            print('Wrong username or password')
             return False
 
     def verifyAccount(self):
         if (self.username != None) and (self._password != None):
+            print('Account Verified')
             return True
         else:
+            print('Account not Verified')
             return False
 
     def forgetPassword(self):
+        print('Your password is ',self._password)
         return self._password
 
     def changePassword(self, new_pw):
         self._password = new_pw
+        print('Password successfully changed')
         return True
-
 class Admin(User):
     def verifyUser(self):
         if (self.username != None) and (self._password != None):
+            print('Account Verified')
             return True
         else:
+            print('Account not Verified')
             return False
 
     def solveThreadProblem(self):
@@ -47,7 +57,6 @@ class Admin(User):
     def verifyPayment(self):
         print("Payment verified!")
         return True
-
 class Siswa(User):
     _siswaId = None
     angkatan = None
@@ -59,14 +68,17 @@ class Siswa(User):
         self.angkatan = angkatan
         self._nomorHp = nomorHp
         self._identificationCard = identificationCard
+        print('Account successfully created')
 
     def bookTutor(self):
+        print('Tutor booked')
         return True
 
-    def sendMessage(self):
-        message = chatMessage('test','test','test')
-        self.message.sendMessage('test')    
-
+    def sendMessage(self, receiver, pesan):    
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        message = chatMessage(self.username,receiver,current_time)
+        message.sendMessage(pesan)  
 class Tutor(User):
     _tutorId = None
     lastEducation = None
@@ -80,15 +92,17 @@ class Tutor(User):
         self._nomorHp = nomorHp
         self._identificationCard = identificationCard
         self.rating = rating
+        print('Account successfully created')
     
     def manageRequest(self):
+        print('Show list')
         return True
     
-    def sendMessage(self):
-        message = chatMessage('test','test','test')
-        self.message.sendMessage('test')
-
-
+    def sendMessage(self, receiver, pesan):    
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        message = chatMessage(self.username,receiver,current_time)
+        message.sendMessage(pesan) 
 class chatMessage(User):
     _senderName = None
     _receiveName = None
@@ -100,15 +114,17 @@ class chatMessage(User):
         self._receiveName = receiveName
         self._datetime = datetime
         self._chatHistory = []
+        print('Message successfully created')
         
     def seeMessage(self):
+        print(self._chatHistory)
         return self._chatHistory
     
     def sendMessage(self, message):
         self._chatHistory.append(message)
-        return self._chatHistory
-
-class tutoringSession:
+        print(message,' From ',self._senderName,' to ', self._receiveName)
+        return self._chatHistory    
+class tutoringSession():
     _sessionId = None
     _mentorName = None
     _siswaName = None
@@ -121,11 +137,12 @@ class tutoringSession:
         self._siswaName = siswaName
         self._tutoringPeriod = None
         self._paymentStatus = False
+        print('Session created')
 
     def bookSchedule(self, date):
         self._tutoringPeriod = date
+        print('Calender = ',date)
         return True
-
 class Payment(tutoringSession):
     _mentorFee = None
     _paymentId = None
@@ -133,15 +150,15 @@ class Payment(tutoringSession):
     def checkOut(self, paymentID, fee):
         self._mentorFee = fee
         self._paymentId = paymentID
+        print('Check out succeed')
         return True
 
     def verifiedBilling(self):
         if (self._paymentId != None):
             self._paymentStatus = True
+            print('Status Active')
         return self._paymentStatus
-
-# nunggu relasi dari tutor ama siswa
-class Thread:
+class Thread():
     _threadId = None
     threadTag = None
     threadHeader = None
@@ -153,31 +170,76 @@ class Thread:
         self.threadTag = threadTag
         self.threadHeader = threadHeader
         self.senderName = senderName
+        print('Thread successfully created')
     
     def replyThread(self, reply):
         self.replies.append(reply)
+        print(reply)
         return True
 
     def removeThread(self):
         self._threadId = None
+        print('Thread successfully removed')
         return True
-
-class Post:
+class Post():
     _threadId = None
     text = None
 
     def writePost(self, text):
         self.text = text
+        print('Post successfully removed')
         return True
 
     def seePost(self):
+        print(self.text)
         return self.text
     
     def deletePost(self):
         self.text = None
+        print('Post successfully deleted')
         return True
-    
-    
-    
+
 class __main___:
-    print("main")
+    # User
+    user = User('User','password','User','user@gmail.com','User')
+    user.login('User','password')
+    user.verifyAccount()
+    user.forgetPassword()
+    user.changePassword('newPassword')
+
+    # Akun Admin
+    admin = Admin('Admin','password','Admin','admin@gmail.com','Admin')
+    admin.verifyAccount()
+    admin.solveThreadProblem()
+    admin.verifyPayment()
+
+    # Akun Siswa
+    siswa = Siswa('Siswa','password','Siswa','siswa@gmail.com','Siswa')
+    siswa.createAccount('S001',2020,'0123456789','identitas siswa')
+    siswa.bookTutor()
+    siswa.sendMessage('Mark','Pesan')
+
+    # Akun Tutor
+    tutor = Tutor('Tutor','password','Tutor','tutor@gmail.com','Tutor')
+    tutor.createAccount('T001','Universitas Gadjah Mada','9876543210','identitas tutor','5')
+    tutor.manageRequest
+    tutor.sendMessage('Luke','Test')
+
+    # Sesi belajar
+    sesi = Payment('001',tutor.name,siswa.name)
+    sesi.bookSchedule(date.today())
+    sesi.checkOut('P001',100000)
+    sesi.verifiedBilling()
+
+    # Thread
+    thread = Thread()
+    thread.createThread('TH001','Matematika','1 + 1 = ','Bill')
+    thread.replyThread('2')
+    thread.removeThread()
+
+    # Post
+    post = Post()
+    post.writePost("Post baru")
+    post.seePost()
+    post.deletePost()
+
